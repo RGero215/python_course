2a9a23e4ab7916c5837418c0074fb37986c20114
RGero215 <rgero215@gmail.com>
2025-09-27 15:44:26 -0400

    add greeting function

--- ramon.py
+++ ramon.py
@@ -1,3 +1,21 @@
-name = 'andy' # a comment
-print(f'hello {name}')
-print(name == 'andy')
+# name = 'andy' # a comment
+# print(f'hello {name}')
+
+def greeting(name, surname=''):
+    # this function returns a greeting message
+    return f'hello {name} {surname}'
+
+# print(greeting('ramon', 'geronimo'))
+# print(greeting('alice', 'wonderland'))
+# print(greeting('bob'))
+
+greeting_ramon = greeting('ramon', 'geronimo')
+# print(greeting_ramon)
+
+# ...existing code...
+
+def login(greeting_message):
+    print(greeting_message)
+
+login(greeting_ramon)
+# ...existing code...
